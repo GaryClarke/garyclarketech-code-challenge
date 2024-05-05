5c090ad3ac130756b4f540bb594d000a7ed835f8
Gary Clarke <garyclarke1974@gmail.com>
2024-05-05 07:28:31 +0100

    Create problem-detail challenge

--- php-api-pro/problem-detail.php
+++ php-api-pro/problem-detail.php
@@ -1,2 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * Challenge:
+ *
+ * Given a status code, such as $statusCode = 403, write code to retrieve
+ * the corresponding URL that provides information about the problem.
+ * Use the ProblemDetail enum below to accomplish this task.
+ *
+ * Courses: https://www.garyclarke.tech/p/php-api-pro
+ * | https://www.garyclarke.tech/p/learn-object-oriented-php
+ */
+enum ProblemDetail: int
+{
+    case BAD_REQUEST = 400;
+    case UNAUTHORIZED = 401;
+    case FORBIDDEN = 403;
+    // ...
+
+    public function type(): string
+    {
+        return match ($this) {
+            self::BAD_REQUEST => 'https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1',
+            self::UNAUTHORIZED => 'https://datatracker.ietf.org/doc/html/rfc7235#section-3.1',
+            self::FORBIDDEN => 'https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.3'
+        };
+    }
+}
+
+$statusCode = 403;
