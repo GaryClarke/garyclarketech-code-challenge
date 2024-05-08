624d0724678d1c198b3abff91072792dc1006d29
Gary Clarke <garyclarke1974@gmail.com>
2024-05-08 15:56:43 +0100

    Add docblock to the problem

--- phpoop/serializing-objects.php
+++ phpoop/serializing-objects.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 require_once 'vendor/autoload.php';
 
+/**
+ * How can you modify the Config class
+ * so that it can be serialized into the format
+ * you see on line 25.
+ *
+ * Notice that the password has not been serialized 😉
+ *
+ * Course: https://www.garyclarke.tech/p/learn-object-oriented-php
+ */
 class Config
 {
     public function __construct(
@@ -16,5 +25,4 @@ class Config
 $config = new Config('https://some-host', 'super-secret-password');
 
 // 👉 O:6:"Config":1:{s:4:"host";s:17:"https://some-host";}
-
-
+serialize($config);
