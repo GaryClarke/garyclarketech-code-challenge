caf732c77ecd29a56d2027d5fa02cd8c89b3d0f7
Gary Clarke <garyclarke1974@gmail.com>
2024-05-08 19:35:51 +0100

    Create pagination-links challenge

---
+++ php-api-pro/pagination-links.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$totalItems = 35;
+$page = 1;
+$itemsPerPage = 10;
+$totalPages = (int) ceil($totalItems / $itemsPerPage);
+$path = '/flights/JF1001-20250101/reservations';
+
+$links = [
+    'self'  => sprintf('%s?page=%d&itemsPerPage=%d', $path, $page, $itemsPerPage),
+    'first' => sprintf('%s?page=1&itemsPerPage=%d', $path, $itemsPerPage),
+    'last'  => sprintf('%s?page=%d&itemsPerPage=%d', $path, $totalPages, $itemsPerPage),
+    'prev'  => $page > 1 ? sprintf('%s?page=%d&itemsPerPage=%d', $path, $page - 1, $itemsPerPage) : null,
+    'next'  => "👀 WHAT GOES HERE?"
+];
+
+// The result will look like this for page 1
+$links = [
+    'self' => '/flights/JF1001-20250101/reservations?page=1&itemsPerPage=10',
+    'first' => '/flights/JF1001-20250101/reservations?page=1&itemsPerPage=10',
+    'last' => '/flights/JF1001-20250101/reservations?page=4&itemsPerPage=10',
+    'prev' => null,
+    'next' => '/flights/JF1001-20250101/reservations?page=2&itemsPerPage=10'
+];
+
