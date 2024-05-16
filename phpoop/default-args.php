c0b679842516348ed35bce960835de18ca1be2b5
Gary Clarke <garyclarke1974@gmail.com>
2024-05-16 18:43:17 +0100

    Update challenge

--- phpoop/default-args.php
+++ phpoop/default-args.php
@@ -2,19 +2,22 @@
 
 /**
  * Instantiate the Route class by providing ONLY the $name and $methods args
- * The defaults will be used for all other values
+ * Use these values: $name = 'home', $methods = ['GET', 'POST']
+ *
+ * The defaults will be used for all other values.
  *
  * Course: https://www.garyclarke.tech/p/learn-object-oriented-php
  */
 class Route
 {
     public function __construct(
-        ?string $path = '/',
-        ?string $name = null,
-        array $options = [],
-        array $defaults = [],
-        array $methods = [],
-        array $schemes = []
+        private ?string $path = '/',
+        private ?string $name = null,
+        private array $options = [],
+        private array $defaults = [],
+        private array $methods = [],
+        private array $schemes = []
     ) {
     }
 }
+
